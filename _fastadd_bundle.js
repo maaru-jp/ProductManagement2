2d6c95760eb5fbf5ee463e11f3369d17324ca7c2
maaru-jp <33389000+maaru-jp@users.noreply.github.com>
2026-06-08 21:16:53 +0800

    Add files via upload

---
+++ _fastadd_bundle.js
@@ -0,0 +1,141 @@
+﻿<!DOCTYPE html>
+<html class="no-js" lang="zh-Hant-TW">
+<head>
+    <!-- 宣告本網站語言使用UTF-8編碼 -->
+    <meta charset="UTF-8" />
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <!--  7-ELEVEN 交貨便  -->
+    <title>7-ELEVEN 交貨便</title>
+    <!-- OG tag -->
+    <meta property="og:url" content="https://myship.7-11.com.tw" />
+    <meta property="og:type" content="website" />
+    <meta property="og:title" content="7-ELEVEN交貨便" />
+    <meta property="og:description" content="交貨便-就近寄，方便取，最安心的寄取件服務，提供完整包裹貨態，不定期舉辦寄取件優惠活動，讓您隨時掌握最新資訊!" />
+    <meta property="og:image" content="/Images/MyShip/OG_image.jpg" />
+    <meta name="description" content="賣貨便" />
+    <meta name="HandheldFriendly" content="True" />
+    <meta name="MobileOptimized" content="360" />
+    <!-- 宣告網頁的viewport的寬度, 初始縮放為1 => initial-scale=1, 最大縮放為1（即不準縮放避免縮放產生錯位) => maximum-scale=1, 不允許user在手機縮放 user-scalable=0 -->
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0" />
+    <!-- 正常情況下的Meta normal Meta -->
+    <meta name="keyword" content="拍賣,交貨便,C2C,金流,物流,平台" />
+    <meta name="copyright" content="2026 President Chain Store Corporation" />
+    <meta name="theme-color" content="#ffffff">
+    <!-- favicon-->
+    <link rel="icon" type="assets/images/x-icon" href="/Images/MyShip/ico/favicon.ico">
+    <link href="/Styles/LayoutMyShip.css?t=20260114" rel="stylesheet" />
+</head>
+<body class="home rwd ">
+    <div class="cs-l-container">
+        <div class="marginTopSpace"></div>
+        <div class="o-menuMask"></div>
+        <div id="msg" style="display: none;"></div>
+        <!--交貨便內嵌式網頁-->
+        <div class="iframe-container">
+            <iframe src="" style="width: 100%; height: 900px"></iframe>
+        </div>
+        <footer class="footer">
+            <section class="f-recommandLink">
+                <div class=" f-recommandCompany">
+                    <div class="footerWrap f-recommandFlex pt-30">
+                        <div>
+                            <ul class="corp_logo">
+                                <li class="hidden-phone"><img src="/Images/MyShip/seven_logo.png" alt="" class="f-recommandLinkLogo"></li>
+                                <li><a href="/Home/Main"><img style="width:250px" src="/Images/MyShip/logo_white.png" alt="" class="f-logo-white"></a></li>
+                            </ul>
+                            <div class="cus_time">
+                                <div><a href="https://eservice.7-11.com.tw/e-tracking/search.aspx">快速查件點這裡</a></div>
+                                <div>服務時間：週一至週五09:00~18:00 (例假日休息)</div>
+                                <div>交貨便常溫C2C：711ec@sp88.com</div>
+                                <div>郵局便利包、國際交貨便、冷凍C2C：711ec01@sp88.com</div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <span class="hrSpan"></span>
+                <div class="footerWrap f-recommandFlex footer_all_rights_reserve">
+                    <div>
+                        <a href="#" data-toggle="modal" onclick="PopWindow()">隱私權保護政策</a>
+                        <p>
+                            © 2020 President Information CORP. All Rights Reserved
+                        </p>
+                    </div>
+                    <div class="allRightReserve hidden-phone">
+                        <p>瀏覽器建議IE版本11與Chrome螢幕解析度1280x800以上</p>
+                    </div>
+                </div>
+            </section>
+        </footer>
+
+        <div class="modal fade text-left" id="BulletinModal" name="BulletinModal" data-wordcheck="false" data-backdrop="static" data-keyboard="false" tabindex="-1" role="dialog"
+             aria-labelledby="myBulletinModalLabel" style="display: none;" aria-hidden="true">
+            <div class="modal-dialog modal-dialog-scrollable" role="document">
+                <div class="modal-content">
+                    <div class="modal-header">
+                        <h3 class="modal-title"></h3>
+                        <button type="button" class="close rounded-pill" data-dismiss="modal" aria-label="Close">
+                            <i class="bx bx-x"></i>
+                        </button>
+                    </div>
+                    <div class="modal-body">
+                        交貨便服務由統一超商股份有限公司及其履行輔助人（包含但不限於統一數網股份有限公司、大智通文化行銷股份有限公司）（以下統稱統一超商）為提供給消費者更多完善及多元之服務，對於消費者所提供之個人資料，將依個人資料保護法之規定來使用、保管消費者之個人資料。<br>
+                        交貨便服務服務的範圍僅提供居住在中華民國台澎金馬地區，消費者使用交貨便服務，即代表消費者同意提供予交貨便之個人資料，將依中華民國之個人資料保護法規定來蒐集、處理、利用消費者之個人資料，並維護消費者的隱私權。 同時，交貨便在此對於消費者個人資料之使用蒐集將依下列聲明為蒐集、處理、利用。<br>
+                        <ol>
+                            <li>當消費者使用本服務及商品時，統一超商基於服務之提供或商品之配送，將會請消費者填寫服務所需之相關資料(EX.申請表、商品配送資料、退款、兌換贈品等或基於服務需要)，而取得個人資料（EX.姓名、電話、住址、配送地 址、email）。僅將消費者的該些資料使用在消費者所需的個別服務上。</li>
+                            <li>當消費者使用本服務時，統一超商基於服務之提供或商品之銷售，將會請消費者填寫服務所需之相關資料(EX.服務申請、商品配送資料、退款、兌換贈品等活動或基於服務需要)，而取得消費者之個人資料（EX.姓名、電話、住址、配送地 址、email、帳務資料如商品交易資料、憑證、金融帳號、匯款明細…等）。統一超商於本地，蒐集、處理、利用消費者您所提供之個人資料，僅作為本服務所需之使用的（包含但不限於聯絡、確認身分、通知取件、發送簡訊或電話通知延後取件、贈品兌換簡訊、排除客訴爭議、確認貨品是否送達或取件、遺失賠償、毀損補償、確認配送或取件商品內容物等）。</li>
+                            <li>個人資料利用期間為消費者提供日起直至消費者提出停止使用或統一超商停止提供服務之日為止（須待寄件者、收件者與統一超商間已完全無爭議）。消費者的個人資料使用區域僅限於本地地區內使用，並為提供消費者更完善、更多元的服務以及更好之服務品質，消費者同意統一超商為履行本服務下，將消費者的資料提供給合作之配送業者、簡訊發送業者如中華電信或關係企業（EX.捷盛運輸股份有限公司、統一速達股份有限公司、統一超商門市端、統一數網股份有限公司、統一資訊股份有限公司等），以提供消費者所需的個別服務、行銷活動及相關優惠訊息。</li>
+                            <li>統一超商依所提供之服務需要，可能需請消費者提供的個人資料種類，包括基本資料如姓名、電話、住址、配送地址、email、帳務資料如商品交易資料、憑證、金融帳號、匯款明細…等，其中帳務資料主要使用在遺失賠償、毀損補償及商品價值認定。</li>
+                            <li>消費者的個人資料均被嚴密地保存在資料庫系統中。同時，任何人均需在統一超商訂定之資料授權管理規範下，進行資料之取得與使用，而不在授權範圍內之任何人，均不得亦無法通過授權控管系統而取得資料。統一超商會以防護機制防止不法入侵，避免消費者的個人資料遭到非法存取。</li>
+                            <li>
+                                統一超商絕對不會任意出售、交換、出租或以其他變相之方式，將消費者的個人資料揭露與其他團體或個人。惟有下列三種情形，統一超商會與第三者共用消費者之個人資料：<br>
+                                (1)經過消費者的事前同意或授權允許時。<br>(2)司法單位或其他主管機關經合法正式的程序要求時。<br>(3)為了提供消費者其他服務或優惠權益，需要與提供該服務或優惠之第三者共用消費者的資料時，統一超商會在活動時提供充分說明並告知，消費者可以自由選擇是否接受這項服務或優惠。
+                            </li>
+                            <li>消費者得就其留於統一超商之個人資料依法向統一超商客服單位(請求行使:查詢或請求閱覽、製給複製本、補充或更正、請求停止蒐集、處理或利用、請求刪除等權利。惟上述權利，若因消費者不符合申請程序或法律規定，或統一超商依法負有保存義務，或為必要之軌跡保存，或法律另有規定之情況者，則不在此限。如果消費者就其資料向統一超商請求答覆查詢、提供閱覽或製給複製本時，統一超商將酌收必要成本費用。而前述之申請，應填具申請文件，統一超商得向消費者請求提出可資確認之身分證明文件;若委託他人代為申請者，並應出具委任書，且提供本人及代理人之身分證明文件。請於上班時間聯絡(週一至週五，早上9:00~下午18:00)：02-2721-4234，亦可使用 24 小時傳真服務專線：02-6638-2398，當然也可以來信至服務信箱 711ec@sp88.com。消費者同意如因行使上述權利或選擇不提供作業所需之個人資料，將影響您的本服務權益。</li>
+                            <li>消費者保證提供給統一超商之個人資料，是自己的個人資料或是被授權可以提供給統一超商之個人資料，若有違反保證，由消費者自負法律責任、與統一超商無涉，並對統一超商負損害賠償責任。</li>
+                        </ol>
+                    </div>
+                    <div class="modal-footer justify-content-center">
+                        <button type="button" class="btn btn-primary ml-1" data-dismiss="modal">
+                            <span class="d-block">我知道了</span>
+                        </button>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <script src="/Script/LayoutMyShip.js?t=20260114"></script>
+
+    <script type='text/javascript'>
+        $(document).ready(function () {
+            const url = new URL(window.location.href);
+            const alertMessage = url.searchParams.get('AlertMessage');
+
+            if (alertMessage) {
+                alertify.alert(decodeURIComponent(alertMessage));
+                url.searchParams.delete('AlertMessage');
+                history.replaceState(null, '', url.toString());
+            }
+
+            $.ajax({
+                type: "POST",
+                async: true,
+                url: "/Myship/GetMyshipPageInfo",
+                success: function (result) {
+                    if (result.iFrameUrl) {
+                        $(".iframe-container iframe").attr("src", `https://${result.iFrameUrl}/`);
+                    }
+                    if (result.msg) {
+                        console.log("msg =", result.msg);
+                        $("#msg").text(result.msg);
+                    }
+                },
+                error: function () {
+                    alertify.alert('系統發生錯誤，請稍後再試');
+                }
+            })
+        });
+    </script>
+    <script src="/Scripts/ga.js?t=20250609"></script>
+</body>
+</html>
